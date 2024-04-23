1316e8f82e79b9b544c9f3f9ba2db430469dfd6e
tjdaniel1 <robot2daniel@gmail.com>
2024-04-23 11:37:34 +0900

    JPA commit

--- auth/src/main/java/com/example/auth/AuthApplication.java
+++ auth/src/main/java/com/example/auth/AuthApplication.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableFeignClients @EnableScheduling
@@ -20,6 +22,19 @@ public class AuthApplication {
 	@Bean
 	public PasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+			}
+		};
 	}
 
 }
